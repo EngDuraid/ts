356f24a055e4ab80601f7045afcf3367a9b15ebc
Eng. Duraid <eng.duraid@outlook.com>
2023-04-24 15:07:04 +0300

    Before adding router

---
+++ src/components/events/EventListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ event.name }}</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Event } from "../../models/Event";
+export default defineComponent({
+  name: "EventListItem",
+  props: {
+    event: {
+      type: Object as () => Event,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style scoped></style>
